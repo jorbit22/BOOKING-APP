d90e5c8b4138e74d62cff9166ae4777ca4f939b9
unknown <ZBV>
2024-01-10 17:36:36 +0100

    auth commit

---
+++ frontend/src/components/Hero.tsx
@@ -0,0 +1,14 @@
+const Hero = () => {
+  return (
+    <div className="bg-blue-800 pb-16">
+      <div className="container mx-auto flex flex-col gap-2">
+        <h1 className="text-5x1 text-white font-bold">find your stay</h1>
+        <p className="text-2x1 text-white">
+          search low prices on hotels for your dream vaction...
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
